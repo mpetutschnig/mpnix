14592f8109a6f33a08406540393623369150d7cb
mpetutschnig <55446353+mpetutschnig@users.noreply.github.com>
2026-06-28 13:45:27 +0200

    Update flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Minimalistisches TUI & Hyprland Setup für mp";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, sops-nix, ... }@inputs: {
+    nixosConfigurations.laptop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+        
+        sops-nix.nixosModules.sops
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.extraSpecialArgs = { inherit inputs; };
+          home-manager.users.mp = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
