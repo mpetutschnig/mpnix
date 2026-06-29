29e295f7eaabec2f629477486b9a37456c4a45e8
mpetutschnig <55446353+mpetutschnig@users.noreply.github.com>
2026-06-29 20:47:44 +0200

    Update home.nix

--- home.nix
+++ home.nix
@@ -11,7 +11,7 @@
 
   home.packages = with pkgs; [ foot waybar fuzzel ];
 
-  programs.vscode = {
+  programs.vscode.profiles.default = {
     enable = true;
     extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
